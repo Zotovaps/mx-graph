12e89e415410af2a557baf239f9addaf70d53e44
Skleznev <job.skleznev@gmail.com>
2023-08-04 19:18:06 +0500

    expand in right top space

---
+++ examples/grapheditor/www/js/custom.js
@@ -0,0 +1,44 @@
+mxCellRenderer.prototype.getControlBounds = function (state, w, h) {
+    if (state.control != null) {
+        var s = state.view.scale;
+        var cx = state.getCenterX();
+        var cy = state.getCenterY();
+
+        if (!state.view.graph.getModel().isEdge(state.cell)) {
+            cx = state.x + state.width - w * s;
+            cy = state.y + h * s;
+
+            if (state.shape != null) {
+                // TODO: Factor out common code
+                var rot = state.shape.getShapeRotation();
+
+                if (this.legacyControlPosition) {
+                    rot = mxUtils.getValue(state.style, mxConstants.STYLE_ROTATION, 0);
+                } else {
+                    if (state.shape.isPaintBoundsInverted()) {
+                        var t = (state.width - state.height) / 2;
+                        cx += t;
+                        cy -= t;
+                    }
+                }
+
+                if (rot != 0) {
+                    var rad = mxUtils.toRadians(rot);
+                    var cos = Math.cos(rad);
+                    var sin = Math.sin(rad);
+
+                    var point = mxUtils.getRotatedPoint(new mxPoint(cx, cy), cos, sin,
+                        new mxPoint(state.getCenterX(), state.getCenterY()));
+                    cx = point.x;
+                    cy = point.y;
+                }
+            }
+        }
+
+        return (state.view.graph.getModel().isEdge(state.cell)) ?
+            new mxRectangle(Math.round(cx - w / 2 * s), Math.round(cy - h / 2 * s), Math.round(w * s), Math.round(h * s))
+            : new mxRectangle(Math.round(cx - w / 2 * s), Math.round(cy - h / 2 * s), Math.round(w * s), Math.round(h * s));
+    }
+
+    return null;
+};
